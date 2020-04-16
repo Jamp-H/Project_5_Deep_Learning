819c4dff94db313b008c5927eb57e0412c798aba
Jamp-H <jmh824@nau.edu>
2020-04-16 11:47:11 -0700

    added todos in py file. Minor edits to code

--- project5.py
+++ project5.py
@@ -1,16 +1,27 @@
 import tensorflow as tf
 import numpy as np
+from sklearn.preprocessing import scale
+### TODO:
+### - Add comments and function headers
+### - Edit Regularization parameters
+### - Loop over Regularization parameter(s)
+### - Plot Log loss vs regularization parameter (diff color each set)
+### - Point to show min of each val loss curve
+### - Get best_param_val - reg param that minimized val loss
+### - retrain entire data set on ^^
+### - Make pred on test set
+### - PDF Doc
 
-spam = np.genfromtxt("spam.data", delimiter=" ")
 
 def getNormX(data):
 	xValuesUnscaled = data[:,:-1]
-	mean = xValuesUnscaled.mean(axis=0)
-	std = xValuesUnscaled.std(axis=0)
-	return (xValuesUnscaled-mean)/std
+	X_sc = scale(xValuesUnscaled)
+	return X_sc
+
 
 def getY(data):
 	return data[:,-1]
+
 
 def trainTestSplit(fold, foldArray, x, y):
 	split = {
@@ -24,34 +35,39 @@ def trainTestSplit(fold, foldArray, x, y):
 		yArray[setName] = y[isSet]
 	return split, xArray, yArray
 
+
 def makeNetwork(x, hiddenUnits):
 	inputs = tf.keras.Input(shape=(x.shape[1],))
 	hidden = tf.keras.layers.Dense(
-    		hiddenUnits, activation="sigmoid", use_bias=False)(inputs)
+			hiddenUnits, activation="sigmoid", use_bias=False)(inputs)
 	outputs = tf.keras.layers.Dense(
-   		 1, activation="sigmoid", use_bias=False)(hidden)
+		 1, activation="sigmoid", use_bias=False)(hidden)
 	network = tf.keras.Model(inputs=inputs, outputs=outputs, name="spam_model")
 	network.compile(
-    		optimizer=tf.keras.optimizers.Adam(),
-    		loss=tf.keras.losses.binary_crossentropy,
+			optimizer=tf.keras.optimizers.Adam(),
+			loss=tf.keras.losses.binary_crossentropy,
 	)
 	return network
 
+
 def runNetwork(epochs, xArray, yArray, network):
 	history = network.fit(
-	    	xArray["train"], 
-		yArray["train"],
-	   	epochs=epochs,
-		    verbose=2,
-		    validation_split=0.5)
+			xArray["train"],
+			yArray["train"],
+			epochs=epochs,
+			verbose=2,
+			validation_split=0.5)
 	test_scores = dict(zip(
-    		network.metrics_names,
-    		network.evaluate(xArray["test"], yArray["test"])))
+			network.metrics_names,
+			network.evaluate(xArray["test"], yArray["test"])))
 	history.history
 
+
 def init(data, numFolds, epochs):
+	# Get X (matrix) and Y (vec) data
 	X = getNormX(data)
 	Y = getY(data)
+
 	foldIDArray = np.arange(numFolds)
 	np.random.seed(0)
 	folds = np.random.permutation(np.tile(foldIDArray, len(Y))[:len(Y)])
@@ -59,4 +75,11 @@ def init(data, numFolds, epochs):
 	network = makeNetwork(X, 100)
 	runNetwork(epochs, xArray, yArray, network)
 
-init(spam, 5, 256)
+
+def main():
+	# get spam data
+	spam = np.genfromtxt("spam.data", delimiter=" ")
+
+	init(spam, 5, 256)
+
+main()
